4095ff6c461451e259fea0f2ce66e37fc3b428ad
Greencheezz <anonsandro@gmail.com>
2024-09-26 10:42:53 +0500

    added compression

--- Packer.py
+++ Packer.py
@@ -1,22 +1,33 @@
 import os
 import pickle
-
+import gzip
 
 def Start():
     directory_path = input("Enter directory path: ")
 
-    with open(os.path.join('result.ble'), 'wb') as archive:
-        fileCounter = 0
+    if not os.path.isdir(directory_path):
+        print("Указанный путь не является директорией или не существует.")
+        return
+
+    with open('result.ble', 'wb') as archive:
         for root, dirs, files in os.walk(directory_path):
             currentFolder = os.path.relpath(root, directory_path)
             for file in files:
-                with open(os.path.join(root, file), 'rb') as f:
-                    tmpdata = f.read()
-                    fileStruct = {
-                        'folder': currentFolder,
-                        'name': os.path.splitext(file)[0],
-                        'extension': os.path.splitext(file)[1],
-                        'data': tmpdata
-                    }
-                    pickle.dump(fileStruct, archive)
-            
+                file_path = os.path.join(root, file)
+                try:
+                    with open(file_path, 'rb') as f:
+                        tmpdata = f.read()
+                        fileStruct = {
+                            'folder': currentFolder,
+                            'name': os.path.splitext(file)[0],
+                            'extension': os.path.splitext(file)[1],
+                            'data': tmpdata
+                        }
+                        pickle.dump(fileStruct, archive)
+                except Exception as e:
+                    print(f"Ошибка при обработке файла {file_path}: {e}")
+    compress_gzip('result.ble', 'result.blezip')
+
+def compress_gzip(input_file, output_file):
+    with open(input_file, 'rb') as f_in, gzip.open(output_file, 'wb') as f_out:
+        f_out.writelines(f_in)
--- Unpacker.py
+++ Unpacker.py
@@ -1,20 +1,36 @@
 import os
 import pickle
-
+import tempfile
+import gzip
 
 def Start():
-    archive_path = input("Enter path to archive: ")
+    archive_path = input("Введите путь к архиву: ")
+    output_directory = input("Введите директорию для распаковки файлов: ")
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    # Декомпрессия RLE-архива во временный файл
+    with tempfile.NamedTemporaryFile(delete=False) as temp_decompressed_file:
+        decompress_gzip(archive_path, temp_decompressed_file.name)
+        temp_decompressed_file_path = temp_decompressed_file.name
 
-    with open(archive_path, "rb") as f:
-        trigger = True
-        while trigger:
+    with open(temp_decompressed_file_path, "rb") as f:
+        while True:
             try:
                 data = pickle.load(f)
-                if not os.path.exists("Unpacked"):
-                    os.makedirs("Unpacked")
-                if(not os.path.exists("Unpacked\\" + data["folder"])):
-                    os.makedirs("Unpacked\\" + data["folder"])
-                with open("Unpacked\\" + data["folder"] + "\\" + data["name"] + data["extension"],'wb') as tmpf:
+                folder_path = os.path.join(output_directory, data["folder"])
+                if not os.path.exists(folder_path):
+                    os.makedirs(folder_path)
+                file_path = os.path.join(folder_path, data["name"] + data["extension"])
+                with open(file_path, 'wb') as tmpf:
                     tmpf.write(data["data"])
             except EOFError:
-                trigger = False
+                break
+            except Exception as e:
+                print(f"Произошла ошибка: {e}")
+                break
+    # Удаление временного файла
+    os.remove(temp_decompressed_file_path)
+
+def decompress_gzip(input_file, output_file):
+    with gzip.open(input_file, 'rb') as f_in, open(output_file, 'wb') as f_out:
+        f_out.writelines(f_in)
